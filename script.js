06fb3bb91236358dcbfe21f72068961f029c409a
navgurukul <khushboo20@navgurukul.org>
2023-07-17 02:07:40 +0530

    this is Background-color-change

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction1() {
+  document.getElementById("color").innerHTML = "Hello pinky cxvhjdfjfgudireu";
+  document.getElementById("color").style.color = "white";
+  document.getElementById("color").style.backgroundColor = "blue";
+}
+function myFunction2() {
+  document.getElementById("color").innerHTML = "Hello khushboo";
+  document.getElementById("color").style.color = "white";
+  document.getElementById("color").style.backgroundColor = "red";
+}
+function myFunction3(){
+  document.getElementById("color").innerHTML = "Hello Neha";
+  document.getElementById("color").style.color = "white";
+  document.getElementById("color").style.backgroundColor = "pink";
+}
+
+
+
